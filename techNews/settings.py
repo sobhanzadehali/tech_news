851e0e617a026a4d74215595db417b9735148874
Sobhan Zadehali <99137872+sobhanzadehali@users.noreply.github.com>
2025-05-13 09:56:23 +0330

    small bug fix in cors

--- techNews/settings.py
+++ techNews/settings.py
@@ -38,11 +38,13 @@ INSTALLED_APPS = [
     'news.apps.NewsConfig',
     'rest_framework',
     'django_filters',
+    'corsheaders',
     'drf_spectacular',
 
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -132,5 +134,7 @@ SPECTACULAR_SETTINGS = {
     'DESCRIPTION': 'roshan internship project apis',
     'VERSION': '1.0.0',
     'SERVE_INCLUDE_SCHEMA': False,
-    # OTHER SETTINGS
+
 }
+
+CORS_ALLOW_ALL_ORIGINS = True  # for development only
