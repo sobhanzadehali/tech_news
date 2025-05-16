b5326bea8fb3b646c669a3e67e375ff283e47e99
Sobhan Zadehali <99137872+sobhanzadehali@users.noreply.github.com>
2025-05-16 01:11:02 -0700

    allowed hosts changed for dev

--- techNews/settings.py
+++ techNews/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-wfcu9_v=&q-bku-b#&fk2@p7^sf8shntr+96#!wx_bv@)g(3j5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
